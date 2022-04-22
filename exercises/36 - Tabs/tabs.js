e0951fdbf3e5bc4cb6ce05a8e5bc01b6ba7295ba
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-04-22 14:37:33 -0300

    tabs

--- exercises/36 - Tabs/tabs.js
+++ exercises/36 - Tabs/tabs.js
@@ -1,1 +1,31 @@
-console.log('ya ya wes we get it.. IT WORKS!');
+const tabs = document.querySelector('.tabs');
+const tabButtons = tabs.querySelectorAll('[role="tab"]');
+const tabPanels = Array.from(tabs.querySelectorAll('[role="tabpanel"]'));
+
+function handleTabClick(event) {
+  // hide all tab panels
+  tabPanels.forEach(panel => {
+    panel.hidden = true
+  })
+  // mark all tabs unselected
+  tabButtons.forEach( tab => {
+    tab.setAttribute('aria-selected', false);
+  })
+  // mark clicked tab as selected
+  event.currentTarget.setAttribute('aria-selected', true);
+  // find associated tabPanel and show it
+  const id = event.currentTarget.id;
+
+  // method 1
+  // const tabPanel = tabs.querySelector(`
+  // [aria-labelledby="${id}"]`);
+  // tabPanel.hidden = false;
+
+  // method 2
+  const tabPanel = tabPanels.find(
+    panel => panel.getAttribute('aria-labelledby') === id
+  );
+  tabPanel.hidden = false;
+}
+
+tabButtons.forEach(button => button.addEventListener('click', handleTabClick));
