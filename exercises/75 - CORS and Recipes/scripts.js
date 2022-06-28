28725523c0c3b40bf619afad2da07986691ab5f2
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-06-28 18:53:35 -0300

    75: CORS and recipes
    We can't share data between two domain names(origins), because of security
    issues. The API needs to have a CORS Policy, and we need to have an origin.
    Because of that we can't run it from the file, so we use parcel to serve.
    (we could also use codepen)
    
    Sometimes the page doesn't have a CORS Policy, we could request the data
    from node, php, rails, etc, but it won't work from the browser. So we need a
    cors proxy. You can only use those if you don't have any sensitive data,
    otherwise you need to build your own server.
    
    CORS = crossorigin resource sharing

--- exercises/75 - CORS and Recipes/scripts.js
+++ exercises/75 - CORS and Recipes/scripts.js
@@ -1,2 +1,48 @@
-// The Recipe Puppy API used in the course is broken
-// Please use this replacement API URL "https://recipes.beginnerjavascript.com/api"
+// to install parcel: npm install --save-dev parcel
+// the command wes used didnt work
+
+// to run parcel: npx parcel index.html
+
+const baseEndpoint = 'https://recipes.beginnerjavascript.com/api';
+const corsProxy = 'https://cors-anywhere.herokuapp.com/'
+const form = document.querySelector('form.search')
+const recipesGrid = document.querySelector('.recipes')
+
+async function fetchRecipes(query) {
+  const response = await fetch(`${corsProxy}${baseEndpoint}/?q=${query}`);
+  const data = await response.json();
+  return data;
+}
+
+async function handleSubmit(e) {
+  e.preventDefault();
+  fetchAndDisplay(form.query.value)
+}
+
+async function fetchAndDisplay(query) {
+    // turn the form(el) off
+    form.submit.disabled = true
+    // submit the search
+    const recipes = await fetchRecipes(query);
+    form.submit.disabled = false
+
+    displayRecipes(recipes.results);
+}
+
+function displayRecipes(recipes) {
+  console.log('creating html')
+  const html = recipes.map(recipe => {
+    return `<div class="recipe">
+    <h2>${recipe.title}</h2>
+    <p>${recipe.ingredients}</p>
+    ${recipe.thumbnail && `<img src="${recipe.thumbnail}" alt="${recipe.title}"/>`}
+    <a href="${recipe.href}">View Recipe</a>
+    </div>
+    `
+  })
+  recipesGrid.innerHTML = html.join('');
+}
+
+form.addEventListener('submit', handleSubmit);
+// on page load run with pizza
+fetchAndDisplay('pizza')
