fb3d384c368e9d55033ac9fc0adb1f44ac91138a
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-06-30 10:59:21 -0300

    76: API with dad jokes
    This API sends the response in different formats, so we need to specify
    in the header that we want json. Otherwise it won't be able to parse it.
    We do that by passing the header type as an argument when we fetch.
    
    We use 'arr' as a parameter in randomItemFromArray, so that it won't only
    be coupled to buttonText, we can pass it any array. We make it an utility
    function, a function that is reusable.
    
    We use 'not' as a parameter in order to make the logic that will stop the
    text button from repeating in a sequence. The handleClick runs the
    randomItemFromArray and sets 'not' to the current textContent. If our item
    is equal to the current textContent, our function runs again.

--- exercises/76 - Dad Jokes/jokes.js
+++ exercises/76 - Dad Jokes/jokes.js
@@ -1,3 +1,7 @@
+const jokeButton = document.querySelector('.getJoke');
+const jokeHolder = document.querySelector('.joke p');
+const loader = document.querySelector('.loader');
+
 const buttonText = [
   'Ugh.',
   '🤦🏻‍♂️',
@@ -8,3 +12,52 @@ const buttonText = [
   'please stop',
   'that was the worst one',
 ];
+
+async function fetchJoke() {
+  loader.classList.remove('hidden')
+  jokeButton.classList.add('hidden')
+  const response = await fetch('https://icanhazdadjoke.com/', {
+    headers: {
+      Accept: 'application/json',
+    }
+  });
+  const data = await response.json();
+  loader.classList.add('hidden')
+  jokeButton.classList.remove('hidden')
+
+  return data;
+}
+
+// how I did Random and substituting the content
+// function getRandom() {
+//   let randomNumber = Math.floor(Math.random() * buttonText.length);
+//   return randomNumber
+// }
+
+// async function handleClick() {
+//   const { joke } = await fetchJoke();
+//   jokeHolder.innerHTML = `<p>${joke}</p>`
+//   jokeButton.innerHTML = `<button>${buttonText[getRandom()]}</button>`
+// }
+
+function randomItemFromArray(arr, not) {
+  const item = arr[Math.floor(Math.random() * arr.length)]
+  if(item === not) {
+    console.log('used it before')
+    return randomItemFromArray(arr, not);
+  }
+  return item;
+}
+
+async function handleClick() {
+  const { joke } = await fetchJoke();
+  jokeHolder.textContent = joke
+  jokeButton.textContent = randomItemFromArray(
+    buttonText,
+    jokeButton.textContent
+  );
+}
+
+jokeButton.addEventListener('click', handleClick);
+
+fetchJoke();
