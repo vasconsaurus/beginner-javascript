7af3d5f4d7454fc4a17c9b379d1816ef6618f569
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-06-30 15:05:20 -0300

    77: currency converter
    At first I had some problem fetching the API, I wasn't passing my key
    corretly. It needed to be added to my headers and passed as the second
    argument in fetch.
    
    I tried two other APIs before I got it to work, exchangerate.host worked
    fine, and seems interesting.
    
    GenerateOptions populates our select currencies (to and from): it takes
    an Object, returns an array, and we use join to turn it into one big html.
    We store the function in a variable because we want it to run only once,
    instead of twice.
    
    Everytime we type we convert the rates, but we don't want to fetch the
    rates everytime. We need to cache the rates, so we made ratesByBase Object.
    It will store all of the rates. If we don't have the currency, the object
    is still empty, then we fetch it.
    
    const rate = ratesByBase[from].rates[to] is something like:
    ratesByBase.CAD.rates.USD
    After we got our rate we convert it.
    
    Listening to input on the form, listens to everything on the form.
    So we get the values, convert them, format, and show the user.

--- exercises/77 - Currency Converter/money.js
+++ exercises/77 - Currency Converter/money.js
@@ -1,3 +1,22 @@
+const fromInput = document.querySelector('[name="from_amount"]');
+const fromSelect = document.querySelector('[name="from_currency"]');
+const toSelect = document.querySelector('[name="to_currency"]');
+const form = document.querySelector('.app form');
+const toAmount = document.querySelector('.to_amount')
+const ratesByBase = {};
+
+const endpoint = 'https://api.apilayer.com/exchangerates_data/latest'
+const key = config.KEY;
+
+const myHeaders = new Headers();
+myHeaders.append("apikey", key);
+
+const requestOptions = {
+  method: "GET",
+  redirect: "follow",
+  headers: myHeaders
+};
+
 const currencies = {
   USD: 'United States Dollar',
   AUD: 'Australian Dollar',
@@ -32,3 +51,47 @@ const currencies = {
   ZAR: 'South African Rand',
   EUR: 'Euro',
 };
+
+function generateOptions(options) {
+  return Object.entries(options).map(([currencyCode, currencyName]) =>
+    `<option value="${currencyCode}">${currencyCode} - ${currencyName}</option>`
+  ).join('');
+}
+
+async function fetchRates(base = 'USD') {
+  const res = await fetch(`${endpoint}?base=${base}`, requestOptions);
+  const rates = await res.json();
+  return rates;
+}
+
+async function convert(amount, from, to) {
+  if(!ratesByBase[from]) {
+    console.log(`oh noe ${from} ${to}, going to get them`);
+    const rates = await fetchRates(from);
+    // store them for next time
+    ratesByBase[from] = rates;
+  }
+  // convert amount
+  const rate = ratesByBase[from].rates[to];
+  const convertedAmount = rate * amount;
+  console.log(`${amount} ${from} - ${convertedAmount} ${to}`);
+  return convertedAmount
+}
+
+function formatCurrency(amount, currency) {
+  return Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency,
+  }).format(amount)
+}
+
+async function handleInput(e) {
+  const rawAmount = await convert(fromInput.value, fromSelect.value, toSelect.value);
+  toAmount.textContent = formatCurrency(rawAmount, toSelect.value);
+}
+
+const optionsHTML = generateOptions(currencies);
+fromSelect.innerHTML = optionsHTML
+toSelect.innerHTML = optionsHTML
+
+form.addEventListener('input', handleInput);
