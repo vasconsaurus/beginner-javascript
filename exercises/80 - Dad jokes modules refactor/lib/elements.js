68748ac17c8efd7b3fb3c5bf2d817d6656810b8c
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-07-05 15:02:11 -0300

    80: dad jokes modules refactor - wes
    Mostly the logic is the same as I did, but this time he organized everything
    in folders, just to show how it might be organized in a bigger project.

--- exercises/80 - Dad jokes modules refactor/lib/elements.js
+++ exercises/80 - Dad jokes modules refactor/lib/elements.js
@@ -1,14 +1,3 @@
 export const jokeButton = document.querySelector('.getJoke');
 export const jokeHolder = document.querySelector('.joke p');
 export const loader = document.querySelector('.loader');
-
-export const buttonText = [
-  'Ugh.',
-  '🤦🏻‍♂️',
-  'omg dad.',
-  'you are the worst',
-  'seriously',
-  'stop it.',
-  'please stop',
-  'that was the worst one',
-];
--- exercises/80 - Dad jokes modules refactor/lib/handlers.js
+++ exercises/80 - Dad jokes modules refactor/lib/handlers.js
@@ -1,9 +1,10 @@
-import { jokeHolder, jokeButton, buttonText } from "./elements.js";
-import { fetchJoke } from "./lib.js";
+import { loader, jokeHolder, jokeButton } from "./elements.js";
+import { fetchJoke } from "./index.js";
 import { randomItemFromArray } from "./utils.js";
+import buttonText from "../data/buttonText.js";
 
 export async function handleClick() {
-  const { joke } = await fetchJoke();
+  const { joke } = await fetchJoke(loader);
   jokeHolder.textContent = joke
   jokeButton.textContent = randomItemFromArray(
     buttonText,
